f54633b29ba63ea4ee95edac6c30dad363125c48
Don Bright <hugh.m.bright@gmail.com>
2014-02-28 05:32:24 +0100

    freebsd autoget dependencies

--- tools/get_dependencies.sh
+++ tools/get_dependencies.sh
@@ -12,8 +12,11 @@ get_fedora_deps()
 
 get_freebsd_deps()
 {
- pkg install clang llvm-devel automake autogen libtool \
-  libxml2-devel uuid-devel openssl-devel bash
+ for pkgname in llvm-devel automake autogen libtool \
+  libxml2 ossp-uuid openssl bash; do
+    echo $pkgname
+    pkg install $pkgname
+ done
 }
 
 get_netbsd_deps()
