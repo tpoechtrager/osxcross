f8b828176ddeb228276516d711eb18de2170826a
TheAssassin <theassassin@commandnotfound.org>
2014-05-15 21:23:45 +0200

    Fixing Debian dependencies. Adding standard Linux command line tools:  - bzip2  - gzip  - xz-utils  - sed  - cpio The tools are usually already installed, though they are usually not installed in e.g. a debootstrapped environment or a minimal installation by default.

--- tools/get_dependencies.sh
+++ tools/get_dependencies.sh
@@ -43,7 +43,8 @@ get_mageia_deps()
 get_debian_deps()
 {
  apt-get install -y --force-yes clang llvm-dev automake autogen \
-  libtool libxml2-dev uuid-dev libssl-dev bash patch make
+  libtool libxml2-dev uuid-dev libssl-dev bash patch make \
+  tar xz-utils bzip2 gzip sed cpio
 }
 
 unknown()
