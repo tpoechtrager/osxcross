e108c9eeb6a5021e760238fc4a71d7c9016933be
Thomas Pöchtrager <t.poechtrager@gmail.com>
2016-09-22 20:23:22 +0200

    cpucount.cpp: std::thread::hardware_concurrency() may return 0

--- tools/cpucount.cpp
+++ tools/cpucount.cpp
@@ -5,7 +5,7 @@
  *
  * Requires C++11 or better.
  */
-int main()
-{
-    std::cout << std::thread::hardware_concurrency() << std::endl;
+int main() {
+  unsigned int numcpus = std::thread::hardware_concurrency();
+  std::cout << (numcpus > 0 ? numcpus : 1) << std::endl;
 }
