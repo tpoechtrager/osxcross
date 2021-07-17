bce9988c71c9e3991199e68f206eceec3ec07ffb
Thomas Pöchtrager <t.poechtrager@gmail.com>
2021-07-17 12:07:28 +0200

    gen_sdk_package_pbzx.sh: Build pbzx with -O2 and -Wall

--- tools/gen_sdk_package_pbzx.sh
+++ tools/gen_sdk_package_pbzx.sh
@@ -27,7 +27,8 @@ get_sources https://github.com/tpoechtrager/pbzx.git master
 if [ $f_res -eq 1 ]; then
   pushd $CURRENT_BUILD_PROJECT_NAME &>/dev/null
   mkdir -p $TARGET_DIR_SDK_TOOLS/bin
-  verbose_cmd $CC -I $TARGET_DIR/include -L $TARGET_DIR/lib pbzx.c \
+  verbose_cmd $CC -O2 -Wall \
+              -I $TARGET_DIR/include -L $TARGET_DIR/lib pbzx.c \
               -o $TARGET_DIR_SDK_TOOLS/bin/pbzx -llzma -lxar \
               -Wl,-rpath,$TARGET_DIR/lib
   build_success
