9a63001170f0b0135a8d56421b51e00f8a6d4352
Rémi Verschelde <rverschelde@gmail.com>
2021-07-27 08:54:58 +0200

    compiler_rt: Build arm64 arch when targeting SDK 11.0+
    Fixes #258.

--- build_compiler_rt.sh
+++ build_compiler_rt.sh
@@ -69,6 +69,11 @@ then
   exit 1
 fi
 
+export OSX_ARCHS="x86_64;x86_64h"
+if [ $(osxcross-cmp $SDK_VERSION ">=" 11.0) -eq 1 ]; then
+  export OSX_ARCHS="$OSX_ARCHS;arm64"
+fi
+
 HAVE_OS_LOCK=0
 
 if echo "#include <os/lock.h>" | xcrun clang -E - &>/dev/null; then
@@ -132,6 +137,8 @@ if [ $f_res -eq 1 ]; then
     pushd build &>/dev/null
 
     CC=$(xcrun -f clang) CXX=$(xcrun -f clang++) $CMAKE .. \
+      -DDARWIN_osx_ARCHS="$OSX_ARCHS" \
+      -DDARWIN_osx_BUILTIN_ARCHS="$OSX_ARCHS" \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_SYSTEM_NAME=Darwin \
       -DCMAKE_LIPO=$(xcrun -f lipo) \
