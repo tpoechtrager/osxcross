e59a63461da2cbc20cb0a5bbfc954730e50a5472
Thomas Pöchtrager <t.poechtrager@gmail.com>
2021-10-26 09:12:59 +0200

    Fix for #310

--- build_compiler_rt.sh
+++ build_compiler_rt.sh
@@ -127,8 +127,10 @@ if [ $f_res -eq 1 ]; then
     $SED -i 's/${CMAKE_COMMAND} -E ${COMPILER_RT_LINK_OR_COPY}/ln -sf/g' \
       lib/builtins/CMakeLists.txt
 
-    $SED -i 's/list(APPEND ORC_CFLAGS -I${DIR})//g' \
-      lib/orc/CMakeLists.txt
+    if [ -f "lib/orc/CMakeLists.txt" ]; then
+      $SED -i 's/list(APPEND ORC_CFLAGS -I${DIR})//g' \
+        lib/orc/CMakeLists.txt
+    fi
 
     if [ $HAVE_OS_LOCK -eq 0 ]; then
       $SED -i "s/COMPILER_RT_HAS_TSAN TRUE/COMPILER_RT_HAS_TSAN FALSE/g" \
