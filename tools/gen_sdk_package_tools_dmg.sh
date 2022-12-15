029f9149dbbef1c99c19d014eba77da6705d8427
Antoine Poinsot <darosior@protonmail.com>
2022-12-15 16:03:58 +0100

    tools: allow to checkout a specific revision when building pbzx

--- tools/gen_sdk_package_tools_dmg.sh
+++ tools/gen_sdk_package_tools_dmg.sh
@@ -37,7 +37,8 @@ require cpio
 build_xar
 
 # build pbzx
-get_sources https://github.com/tpoechtrager/pbzx.git master
+PBZX_REVISION="${PBZX_REVISION:-"master"}"
+get_sources https://github.com/tpoechtrager/pbzx.git "$PBZX_REVISION"
 
 if [ $f_res -eq 1 ]; then
   pushd $CURRENT_BUILD_PROJECT_NAME &>/dev/null
